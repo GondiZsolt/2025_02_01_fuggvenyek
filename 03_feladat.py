20301c3c3f5c38768ddafcf84ecce7691b7842df
Gondi Zsolt <GondiZsolt@pg.local>
2025-02-01 09:48:33 +0100

    change shi

--- 03_feladat.py
+++ 03_feladat.py
@@ -3,32 +3,32 @@ amely a paraméterként átvett listában megvizsgálja, hogy hány darab három
 és ezzel az értékkel tér vissza! A program tartalmazza a függvény hívását is!"""
 
 
-# def harommal_oszthatok(a, b, c):
-#  eredmeny = 0
-#  if a % 3 == 0:
-#   eredmeny += 1
-#  if b % 3 == 0:
-#   eredmeny += 1
-#  if c % 3 == 0:
-#   eredmeny += 1
-#  return eredmeny
+def harommal_oszthatok(a, b, c):
+ eredmeny = 0
+ if a % 3 == 0:
+  eredmeny += 1
+ if b % 3 == 0:
+  eredmeny += 1
+ if c % 3 == 0:
+  eredmeny += 1
+ return eredmeny
 
-# print(harommal_oszthatok(3, 6, 9))
-# print(harommal_oszthatok(3, 6, 10))
-# print(harommal_oszthatok(3, 7, 11))
-# print(harommal_oszthatok(3, 12, 15))
-
-
-
-
-def harommal_oszthaok(szamok):
+print(harommal_oszthatok(3, 6, 9))
+print(harommal_oszthatok(3, 6, 10))
+print(harommal_oszthatok(3, 7, 11))
+print(harommal_oszthatok(3, 12, 15))
 
 
 
-print(harommal_oszthatok([3, 6, 9]))
-print(harommal_oszthatok([3, 6, 10]))
-# print(harommal_oszthatok(3, 7, 11))
-# print(harommal_oszthatok(3, 12, 15))
+
+# def harommal_oszthaok(szamok):
+
+
+
+# print(harommal_oszthatok([3, 6, 9]))
+# print(harommal_oszthatok([3, 6, 10]))
+# # print(harommal_oszthatok(3, 7, 11))
+# # print(harommal_oszthatok(3, 12, 15))
 
 
 """Alakítsd át az előző programot úgy, hogy a felhasználó által megadott számokat tárolja el a program egy listában,
